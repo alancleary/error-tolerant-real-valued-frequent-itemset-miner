5793dcfba2edbcea1c9a577ec319ed590edd1341
Alan Cleary <alancleary@comcast.net>
2014-11-10 15:18:45 -0700

    Fleshed out more of the AFI code

--- AFI/afi.py
+++ AFI/afi.py
@@ -2,32 +2,47 @@
 
 import math
 
-# takes a matrix D, row error ration epsilon_r, column error threshold epsilon_c, and and minimum support threshold minsup
+# takes a matrix D, row error ratio epsilon_r, column error ratio epsilon_c, and minimum support threshold minsup
 def approximate_frequent_itemsets( D, epsilon_r, epsilon_c, minsup ):
     T = []
+    L = [[]]
     for i in range( 0, len( D[ 0 ] ) ):
         T[ i ] = __gen_support( D, i )
+        L[ 0 ].apend( i )
     k = 1
-    # L_1 = union for i > 0 through m of {i}
+    AFI_p = ()
     while # L_k is not empty:
         k++
-        # L_k = __generate_candidate_itemset( L_(k-1), minsup^(k-1) )
+        previous_minsup_k = math.max( minsup*(1-(((k-1)*epsilon_c)/(math.floor((k-1)*epsilon_r)+1))), 0 )
+        L[ k ] = __generate_candidate_itemset( L[ k-1 ], previous_minsup )
         if math.floor( k*epsilon_r ) == math.floor( (k+1)*epsilon_r ):
-            # T( L_k ) = __one_extension( I, L_(k-1) )
+            T[ L[ k ] ] = __one_extension( I, L[ k-1 ] )
         else:
-            # T( L_k ) = __zero_extension( I, L_(k-1) )
-        # AFI_p = AFI_p union L_k
-    # AFI = __filter( AFI_p, minsup, epsilon_c )
+            T[ L[ k ] ] = __zero_extension( I, L[ k-1 ] )
+        AFI_p = AFI_p.union( L[ k ] )
+    AFI = __filter( AFI_p, minsup, epsilon_c )
     return AFI
 
+# generates the support for each singleton item
 def __gen_support( D, i ):
     pass
 
-def __generate_candidate_itemset( L, minsup ):
-    pass
+def __generate_candidate_itemset( previous_L, previous_minsup ):
+    L = []
+    return L
 
-def __one_extension( I, L ):
-    pass
+# generates a transaction set for the case where additional error will not be tolerated
+def __one_extension( I, previous_L ):
+    # the transaction set of a (k+1) itemset I is the intersection of the transaction sets of its length k subsets
+    T = ()
+    return T
 
-def __zero_extension( I, L ):
-    pass
+# generates a transaction set for the case where additional error will be tolerated
+def __zero_extension( I, previous_L ):
+    # the transaction set of a (k+1) itemset I is the union of the transaction sets of its length k subsets
+    T = ()
+    return T
+
+def filter( AFI_p, minsup, epsilon_c ):
+    AFI = ()
+    return AFI
