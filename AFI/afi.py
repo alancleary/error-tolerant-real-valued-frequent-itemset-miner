fff6c8f72368d30db7d5b0cd299461e56671248c
Alan Cleary <alancleary@comcast.net>
2014-11-16 20:20:40 -0700

    Added some (helpful?) comments

--- AFI/afi.py
+++ AFI/afi.py
@@ -4,20 +4,28 @@ import math
 
 # takes a matrix D, row error ratio epsilon_r, column error ratio epsilon_c, and minimum support threshold minsup
 def approximate_frequent_itemsets( D, epsilon_r, epsilon_c, minsup ):
+    # will hold the support set for each I
     T = []
+    # holds the set of itemsets at each level k
     L = [[]]
+    # the current level
+    k = 0
+    # generate each item's initially support
     for i in range( 0, len( D[ 0 ] ) ):
         T[ i ] = __gen_support( D, i )
-        L[ 0 ].apend( i )
-    k = 1
+        # each set at the first level holds a single item
+        L[ k ].append( ( i ) )
+    # the pre-processed AFIs
     AFI_p = ()
     while # L_k is not empty:
         k++
-        previous_minsup_k = math.max( minsup*(1-(((k-1)*epsilon_c)/(math.floor((k-1)*epsilon_r)+1))), 0 )
+        previous_minsup_k = math.max( minsup*(1-(((k)*epsilon_c)/(math.floor((k)*epsilon_r)+1))), 0 )
         L[ k ] = __generate_candidate_itemset( L[ k-1 ], previous_minsup )
         if math.floor( k*epsilon_r ) == math.floor( (k+1)*epsilon_r ):
+            # how do we generate I?
             T[ L[ k ] ] = __one_extension( I, L[ k-1 ] )
         else:
+            # ditto?
             T[ L[ k ] ] = __zero_extension( I, L[ k-1 ] )
         AFI_p = AFI_p.union( L[ k ] )
     AFI = __filter( AFI_p, minsup, epsilon_c )
