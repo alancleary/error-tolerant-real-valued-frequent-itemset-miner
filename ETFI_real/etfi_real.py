c6a99b23e824cd540c27a57fb812c35265427d7d
Alan Cleary <alan.cleary@msu.montana.edu>
2014-11-28 22:35:49 -0700

    It seems to be working

--- ETFI_real/etfi_real.py
+++ ETFI_real/etfi_real.py
@@ -1,138 +1,124 @@
-# this is an implementation of Approximate Frequent Itemsets (AFI)
+# this is an implementation of Approximate Frequent Itemsets (ETFI)
 
-d = [[2.0, 2.1, 8.0, 2.0, 2.0],[2.1, 2.2, 2.2, 2.2, 2.2],[4.0, 4.0, 9.0, 4.0, 4.0],[6.5, 6.6, 6.5, 20.0, 6.5],[8.0, 20.0, 8.8, 8.0, 8.0],[9.0, 20.0, 9.1, 10.0, 9.1],[3.2, 3.0, 8.0, 20.0, 3.2],[2.0, 2.0, 2.0, 2.0, 2.0]]
+#d = [[2.0, 2.1, 8.0, 2.0, 2.0],[2.1, 2.2, 2.2, 2.2, 2.2],[4.0, 4.0, 9.0, 4.0, 4.0],[6.5, 6.6, 6.5, 20.0, 6.5],[8.0, 20.0, 8.8, 8.0, 8.0],[9.0, 20.0, 9.1, 10.0, 9.1],[3.2, 3.0, 8.0, 20.0, 3.2],[2.0, 2.0, 2.0, 2.0, 2.0]]
+d = [[0,0,0,0,0,0,0,0,0,0],
+    [0,5.1,5.2,5.2,2.0,0,0,0,0,0],
+    [0,2.2,2.2,2.1,2.0,0,0,0,0,0],
+    [0,2.0,5.0,2.2,2.0,0,0,0,0,0],
+    [0,0,0,0,0,10.5,10.6, 0,0,0],
+    [0,0,0,0,0,0,0,0.1,0.1,0.2],
+    [0,0,0,0,0,0,0,0.05,0.06,0.05],
+    [0,0,0,0,0,0,0,0.13,0.12,0.11],
+    [0,0,0,0,0,0,0,0.11,0.11,0.12]]
 
 def example():
-    print "input"
-    print "epsilon_r"
-    e_r = 1.0/3
-    print e_r
-    print "epsilon_c"
-    e_c = 1.0/3
-    print e_c
-    print "matrix D"
-    print d
-    rAFIs = etfi_real(d, e_r, e_c, 5, .5, .5)
-    print "output"
-    print "rAFIs"
-    print rAFIs
+    ETFIs = etfi_real(d, 0.25, 0.25, 2, 0.1, 0.1)
+    print "ETFIs"
+    print ETFIs
 
 import math
 
 # takes a matrix D, row error ratio epsilon_r, column error ratio epsilon_c, and minimum support threshold minsup
 def etfi_real( D, epsilon_r, epsilon_c, range_support, alpha, minsup ):
-    # holds the support set (list of row indices) for each itemset I
-    T = {}
-    # holds the set of itemsetS at EACH level k
-    L = [[]]
-    # the current level
-    k = 0
-    # generates the support set (list of row indices) for each item (column)
+    T = {} # holds the support set (list of row indices) for each itemset I
+    L = [[]] # holds the set of itemsetS at EACH level k
+    E = [] # for each transaction, tells us whether positive or negative values are errors with respect to each itemset
+    k = 0 # the current level
     n = len( D )
-    for i in range( 0, len( D[ 0 ] ) ):
-        # I needs to be frozen so it's immutable and can be used as a key in T
-        itemset = frozenset([i])
-        # each set at the first level holds a single item
-        L[ k ].append( itemset )
-        # we need to know each item's support
-        T[ itemset ] = __gen_support( D, i )
-    # the set of AFIs before filtering
-    AFI_p = []
-    # until no more potential AFIs have been generated
-    minsup_k = n*minsup
+    m = len( D[ 0 ] )
+    # all transactions meet the minsup criteria for each singleton
+    for i in range( n ):
+        E.append( {frozenset([ i ]): 0 for i in range( m )} )
+        for j in range( m ):
+            E[ i ][ frozenset([ j ]) ] = (1 if D[ i ][ j ] >= 0 else -1)
+
+    all_transactions = set([ i for i in range( n ) ])
+    for i in range( 0, m ):
+        itemset = frozenset([i]) # I needs to be frozen so it's immutable and can be used as a key in T
+        rs = __range_support( itemset, all_transactions, D, E )
+        # check that the itemset meets the rangesupport measure
+        if rs >= range_support:
+            L[ k ].append( itemset )
+            T[ itemset ] = all_transactions
+    ETFI = [] # the set of ETFIs before filtering
+    # until no more potential ETFIs have been generated
     while L[ k ]:
         k += 1
         # calculate this iteration's minsup
-        minsup_k = n*max( minsup*(1-(((k+1)*epsilon_c)/(math.floor((k+1)*epsilon_r)+1))), 0 )
+        minsup_k = max( minsup*(1-((k*epsilon_c)/(math.floor(k*epsilon_r)+1))), 0 )
         # generate the set of itemsets for level k
-        itemsets_and_subsets = __generate_candidate_itemsets( L[ k-1 ], T, minsup_k, D, epsilon_r, epsilon_c )
-        print "pre extension"
-        print itemsets_and_subsets
+        itemsets_and_subsets = __generate_candidate_itemsets( L[ k-1 ] )
         # if another 0 is allowed in the itemset
         if math.floor( (k)*epsilon_r ) == math.floor( (k+1)*epsilon_r ):
-           __one_extension( itemsets_and_subsets, T )
+            __one_extension( itemsets_and_subsets, T )
+            __generate_support( itemsets_and_subsets.keys(), T, D, E, epsilon_r, alpha )
         # if another zero is not allowed in the itemset
         else:
-            __zero_extension( itemsets_and_subsets, T, D, epsilon_r )
-        # the remaining itemsets are AFIs
+            __zero_extension( itemsets_and_subsets, T, all_transactions )
+            __generate_support( itemsets_and_subsets.keys(), T, D, E, epsilon_r, alpha )
+        # filter the itemsets using the range_support criteria
+        for itemset in itemsets_and_subsets.keys():
+            rs = __range_support( itemset, T[ itemset ], D, E )
+            if rs < range_support or len( T[ itemset ] ) < n*minsup_k:
+                del T[ itemset ]
+                del itemsets_and_subsets[ itemset ]
+        # the remaining itemsets are ETFIs
         L.append( itemsets_and_subsets.keys() )
-        # add the itemsets to the global list of AFIs and remove their subsets
+        # add the itemsets to the global list of ETFIs and remove their subsets
         for itemset in itemsets_and_subsets.keys():
             for subset in itemsets_and_subsets[ itemset ]:
-                if subset in AFI_p:
-                    AFI_p.remove( subset )
-            AFI_p.append( itemset )
-    #AFI = __filter( AFI_p, minsup, epsilon_c, T, n )
-    AFI = AFI_p
-    return AFI
+                if subset in ETFI:
+                    ETFI.remove( subset )
+            ETFI.append( itemset )
+    return ETFI
 
-# generates the support set (list of rows indices) for each singleton item (column)
-def __gen_support( D, j ):
-    rows = set()
-    for i in range( len( D ) ):
-        if D[ i ][ j ] == 1:
-            rows.add( i )
-    return rows
+# a function that finds the range_support for a given itemset in a given set of transactions
+def __range_support( itemset, transactions, D, E ):
+    rs = 0
+    for t in transactions:
+        positive = set()
+        negative = set()
+        for i in itemset:
+            if D[ t ][ i ] > 0:
+                positive.add( i )
+            elif D[ t ][ i ] < 0:
+                negative.add( i )
+        # we're assuming the transactions already meet the range criteria
+        # is positive or negative considered the error?
+        if E[ t ][ itemset ] > 0: # negative is the error
+            if len( positive ) > 0:
+                rs += min([ D[ t ][ i ] for i in positive ])
+        elif E[ t ][ itemset ] < 0: # positive is the error
+            rs += min([ abs( D[ t ][ i ] ) for i in negative ])
+        else: # neither is the error
+            rs += max( min([ D[ t ][ i ] for i in positive ]), min([ abs( D[ t ][ i ] ) for i in negative ]) )
+    return rs
 
 # generates all possible length k+1 itemsets given a set of length k itemsets, and then prunes them
-def __generate_candidate_itemsets( previous_L, T, minsup_k, D, epsilon_r, epsilon_c ):
-    n = len( D )
+def __generate_candidate_itemsets( previous_L ):
     # find all possible size k+1 itemsets
     itemsets_and_subsets = {}
     for i in range( 0, len( previous_L )-1 ):
         for j in range( i+1, len( previous_L ) ):
             itemset = previous_L[ i ].union( previous_L[ j ] )
             itemset = frozenset( itemset )
+
             if len( itemset ) == len( previous_L[ i ] )+1:
                 if itemset in itemsets_and_subsets:
                     itemsets_and_subsets[ itemset ].add( previous_L[ i ] )
                     itemsets_and_subsets[ itemset ].add( previous_L[ j ] )
                 else:
                     itemsets_and_subsets[ itemset ] = set([ previous_L[ i ], previous_L[ j ] ])
-    print "generated"
-    print itemsets_and_subsets
     # generate all possible size k subsets for each itemset and see if any of them don't support the itemset
     for itemset in itemsets_and_subsets.keys():
         subsets = __generate_subsets( set(), list( itemset ), len( itemset )-1 )
         difference = itemsets_and_subsets[ itemset ].symmetric_difference( subsets )
-        # if a non-AFI was found to be a subset, then the symmetric difference will not be empty
+        # if a non-ETFI was found to be a subset, then the symmetric difference will not be empty
         if difference:
-            print "eliminating for lack of support"
             del itemsets_and_subsets[ itemset ]
-            break
-        # prune itemsets that are not within the row error threshold
-        subset_transactions = [ T[ s ] for s in itemsets_and_subsets[ itemset ] ]
-        support = subset_transactions[ 0 ]
-        for i in range( 1, len( subset_transactions ) ):
-            support.update( subset_transactions[ i ] )
-        if len( support ) < n*minsup_k:
-            print "eliminating for not meeting minsup"
-            print itemset
-            del itemsets_and_subsets[ itemset ]
-            break
-        for t in support:
-            non_error = 0
-            for i in itemset:
-                if D[ t ][ i ] == 1:
-                    non_error += 1
-            if non_error < (1-epsilon_r)*len( itemset ):
-                print "eliminating for row error"
-                print itemset
-                del itemsets_and_subsets[ itemset ]
-                break
-        if itemset not in itemsets_and_subsets:
             continue
-        # prune itemsets that are not within the column error threshold
-        for i in itemset:
-            non_error = 0
-            for t in support:
-                if D[ t ][ i ] == 1:
-                    non_error += 1
-            if non_error < (1-epsilon_c)*minsup_k*n:
-                print "eliminating for column error"
-                print itemset
-                del itemsets_and_subsets[ itemset ]
-                break
+        # remove itemsets that don't meet the minsup criteria?????
+
     # the remaining itemsets are the candidates
     return itemsets_and_subsets
 
@@ -148,6 +134,11 @@ def __generate_subsets( previous, itemset, k ):
         subsets.add( frozenset( previous ) )
     return subsets
 
+# a function that determines if the given transaction and itemset meet the range criteria for alpha
+def __range( transaction, itemset, alpha ):
+    top = max([ abs( transaction[ i ] ) for i in itemset ])
+    bottom = min([ abs( transaction[ i ] ) for i in itemset ])
+    return top-bottom <= alpha*bottom
 
 # generates a transaction set for the case where additional error will not be tolerated
 def __one_extension( itemsets_and_subsets, T ):
@@ -161,32 +152,60 @@ def __one_extension( itemsets_and_subsets, T ):
         T[ itemset ] = t
 
 # generates a transaction set for the case where additional error will be tolerated
-def __zero_extension( itemsets_and_subsets, T, D, epsilon_r ):
-    m = len( D[ 0 ] )
-    # the transaction set of a (k+1) itemset I is the rows in the database that have <= epsilon_r error in their support for I
+def __zero_extension( itemsets_and_subsets, T, all_transactions ):
     for itemset in itemsets_and_subsets.keys():
-        # make a list of the supporting transactions
-        t = set()
-        for i in range( len( D ) ):
-            num_support = 0
-            for j in itemset:
-                if D[ i ][ j ] == 1:
-                    num_support += 1
-            if num_support/m > epsilon_r:
-                t.add( i )
-        T[ itemset ] = t
+        # each itemset is set to all transactions so __generate_support scans the entire database
+        T[ itemset ] = all_transactions
 
-def __filter( AFI_p, minsup, epsilon_c, T, n ):
-    item_support = math.ceil( n*minsup*(1.0-epsilon_c) )
-    AFI = []
-    for itemset in AFI_p:
-        if len( T[ itemset ] ) >= minsup*n:
-            is_AFI = True
-            for item in itemset:
-                support = T[ frozenset([ item ]) ].intersection( T[ itemset ] )
-                is_AFI = len( support ) > item_support
-                if not is_AFI:
-                    break
-            if is_AFI:
-                AFI.append( itemset )
-    return AFI
+# given a set of itemsets and their transactions, this function prunes the transactions that don't meet the range criteria for each itemset
+def __generate_support( itemsets, T, D, E, epsilon_r, alpha ):
+    for itemset in itemsets:
+        max_error = epsilon_r*len( itemset )
+        remove = []
+        for t in T[ itemset ]:
+            positive = set()
+            negative = set()
+            zero = 0
+            for i in itemset:
+                if D[ t ][ i ] > 0:
+                    positive.add( i )
+                elif D[ t ][ i ] < 0:
+                    negative.add( i )
+                else:
+                    zero += 1
+            if zero+len( positive ) <= max_error and zero+len( negative ) <= max_error:
+                positive_max = max( D[ t ][ i ] for i in positive )
+                positive_min = min( D[ t ][ i ] for i in positive )
+                positive_range = ((positive_max-positive_min)*1.0)/positive_min
+                negative_max = max( abs( D[ t ][ i ] ) for i in positive )
+                negative_min = min( abs( D[ t ][ i ] ) for i in positive )
+                negative_range = ((negative_max-negative_min)*1.0)/negative_min
+                if positive_range <= alpha*positive_min and negative_range <= alpha*negative_min:
+                    E[ t ][ itemset ] = 0
+                elif positive_range <= alpha*positive_min:
+                    E[ t ][ itemset ] = 1
+                elif negative_range <= alpha*negative_min:
+                    E[ t ][ itemset ] = -1
+                else:
+                    remove.append( t )
+            elif zero+len( negative ) <= max_error:
+                positive_max = max( D[ t ][ i ] for i in positive )
+                positive_min = min( D[ t ][ i ] for i in positive )
+                positive_range = ((positive_max-positive_min)*1.0)/positive_min
+                if positive_range <= alpha*positive_min:
+                    E[ t ][ itemset ] = 1
+                else:
+                    remove.append( t )
+            elif zero+len( positive ) <= max_error:
+                negative_max = max( abs( D[ t ][ i ] ) for i in positive )
+                negative_min = min( abs( D[ t ][ i ] ) for i in positive )
+                negative_range = ((negative_max-negative_min)*1.0)/negative_min
+                if negative_range <= alpha*negative_min:
+                    E[ t ][ itemset ] = -1
+                else:
+                    remove.append( t )
+            else:
+                remove.append( t )
+        for t in remove:
+            T[ itemset ].remove( t )
+
